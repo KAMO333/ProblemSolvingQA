c1d0db597fb495da35d363b8963e9ec4a574cc34
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-02-07 19:08:08 +0200

    simple

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -95,8 +95,7 @@ def can_double_down(card_one, card_two):
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """ 
-    numbers = [9, 10, 11]
 
-    if value_of_card(card_one) + value_of_card(card_two) in numbers:
+    if value_of_card(card_one) + value_of_card(card_two) in [9, 10, 11]:
         return True
     return False
