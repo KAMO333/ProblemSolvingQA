8a4986dc8330b48800850b463557d2a576c1f597
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-02-08 23:44:49 +0200

    cleanup

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -7,7 +7,6 @@ def capitalize_title(title):
     :param title: str - title string that needs title casing.
     :return: str - title string in title case (first letters capitalized).
     """
-
     pass
 
 
@@ -17,7 +16,6 @@ def check_sentence_ending(sentence):
     :param sentence: str - a sentence to check.
     :return: bool - return True if punctuated correctly with period, False otherwise.
     """
-
     pass
 
 
@@ -27,7 +25,6 @@ def clean_up_spacing(sentence):
     :param sentence: str - a sentence to clean of leading and trailing space characters.
     :return: str - a sentence that has been cleaned of leading and trailing space characters.
     """
-
     pass
 
 
@@ -39,5 +36,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :param new_word: str - replacement word.
     :return: str - input sentence with new words in place of old words.
     """
-
     pass
