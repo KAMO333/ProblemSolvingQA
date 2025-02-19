9e882fc0d998ca89aabb6b582ebd4f241e5b3227
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2025-02-19 09:55:27 +0200

    variable and math (minus)

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,18 @@
+public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+    int minutesInOven = 40;
+
+    public  int expectedMinutesInOven() {
+        return minutesInOven;
+    }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int minutesSpend) {
+        return minutesInOven - minutesSpend;
+    }
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+
+}
