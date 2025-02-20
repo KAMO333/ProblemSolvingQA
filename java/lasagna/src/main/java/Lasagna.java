f57eb828c2aa182a5224c4fa668e7da024fbff93
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2025-02-20 09:00:31 +0200

    refactored code

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,22 +1,18 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
     int minutesInOven = 40;
 
     public  int expectedMinutesInOven() {
         return minutesInOven;
     }
 
-    // TODO: define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int minutesSpend) {
-        return minutesInOven - minutesSpend;
+        return expectedMinutesInOven() - minutesSpend;
     }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
     public int preparationTimeInMinutes(int layers) {
         return layers * 2;
     }
 
-    // TODO: define the 'totalTimeInMinutes()' method
     public int totalTimeInMinutes(int layers, int minutesInOven) {
         return preparationTimeInMinutes(layers) + minutesInOven ;
     }
