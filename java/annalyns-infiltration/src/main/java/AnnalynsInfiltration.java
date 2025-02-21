0174a17c8273d4b2ce860b7cb59fc248282f2ec5
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2025-02-21 09:26:35 +0200

    all tests passed

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -15,6 +15,7 @@ class AnnalynsInfiltration {
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+//        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+        return (!archerIsAwake && petDogIsPresent) || !knightIsAwake && !archerIsAwake && prisonerIsAwake;
     }
 }
