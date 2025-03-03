e1e3426aa6d6520c842bc14e30b18b7d997292d3
Kamogelo <mmopanemoroka@gmail.com>
2025-03-03 16:58:12 +0200

    refactored

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,4 +1,3 @@
 def leap_year(year):
-    if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
-        return True
-    return False
+    return year % 400 == 0 or year % 4 == 0 and year % 100 != 0 
+
