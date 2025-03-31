16b04759ae7674180323b9400a50d939f6558a32
kammopajhb024 <kammopajhb024@student.wethinkcode.co.za>
2025-03-31 15:28:59 +0200

    worked changing char

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -12,7 +12,25 @@ class SqueakyClean {
                 results.append((parts[i].substring(1)));
             }
         }
-        return results.toString();
 
+        StringBuilder newResults = new StringBuilder();
+
+        for (int j = 0; j < results.length(); j++) {
+            if (results.charAt(j) == '4') {
+                newResults.append('a');
+            } else if (results.charAt(j) == '3') {
+                newResults.append('e');
+            } else if (results.charAt(j) == '0') {
+                newResults.append('o');
+            } else if (results.charAt(j) == '1') {
+                newResults.append('l');
+            } else if (results.charAt(j) == '7') {
+                newResults.append('t');
+            } else {
+                newResults.append(results.charAt(j));
+            }
+
+        }
+        return newResults.toString();
     }
 }
