d4680d49faf907ff91f70542e6c5d9ea5b3029be
KAMO333 <mmopanemoroka@gmail.com>
2025-04-04 15:39:36 +0200

    disabled

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -3,11 +3,30 @@ public class CarsAssemble {
     public double productionRatePerHour(int speed) {
         int carsPerHour = 221;
 
-        if (speed == 1) {
-            return carsPerHour;
-        }
-
-        return 0;
+       switch (speed) {
+           case 1:
+               return carsPerHour;
+           case 2:
+               return 2 * carsPerHour;
+           case 3:
+               return 3 * carsPerHour;
+           case 4:
+               return 4 * carsPerHour;
+           case 5:
+               return 5 * carsPerHour;
+           case 6:
+               return 6 * carsPerHour;
+           case 7:
+               return 7 * carsPerHour;
+           case 8:
+               return 8 * carsPerHour;
+           case 9:
+               return 9 * carsPerHour;
+           case 10:
+               return 10 * carsPerHour;
+           default:
+               return 0;
+       }
     }
 
     public int workingItemsPerMinute(int speed) {
