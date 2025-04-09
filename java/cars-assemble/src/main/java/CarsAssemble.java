c009cda67f2af8379e4647fa136963c9c61a85f7
KAMO333 <mmopanemoroka@gmail.com>
2025-04-09 14:31:18 +0200

    if else in java

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -3,30 +3,20 @@ public class CarsAssemble {
     public double productionRatePerHour(int speed) {
         int carsPerHour = 221;
 
-       switch (speed) {
-           case 1:
-               return carsPerHour;
-           case 2:
-               return 2 * carsPerHour;
-           case 3:
-               return 3 * carsPerHour;
-           case 4:
-               return 4 * carsPerHour;
-           case 5:
-               return 5 * carsPerHour;
-           case 6:
-               return 6 * carsPerHour;
-           case 7:
-               return 7 * carsPerHour;
-           case 8:
-               return 8 * carsPerHour;
-           case 9:
-               return 9 * carsPerHour;
-           case 10:
-               return 10 * carsPerHour;
-           default:
-               return 0;
-       }
+        if (speed <= 4) {
+            return speed * carsPerHour;
+        } else if (speed <= 8) {
+            int getCarsPerHour = speed * carsPerHour;
+            return getCarsPerHour * 0.9;
+        } else if (speed == 9) {
+            int getCarsPerHour = speed * carsPerHour;
+            return getCarsPerHour * 0.8;
+        } else if (speed == 10) {
+            int getCarsPerHour = speed * carsPerHour;
+            return getCarsPerHour * 0.77;
+        } else {
+            return 0.0;
+        }
     }
 
     public int workingItemsPerMinute(int speed) {
