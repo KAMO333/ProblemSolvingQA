ed15bb8a11620e2e186d023d8a4b621d0a5537df
KAMO333 <mmopanemoroka@gmail.com>
2025-04-24 15:01:42 +0200

    class variables task 3

--- java/jedliks-toy-car/src/main/java/JedliksToyCar.java
+++ java/jedliks-toy-car/src/main/java/JedliksToyCar.java
@@ -1,5 +1,6 @@
 public class JedliksToyCar {
     public String driven = "Driven 0 meters";
+    public String battery = "Battery at 100%";
 
     public static JedliksToyCar buy() {
         return new JedliksToyCar();
@@ -8,11 +9,11 @@ public class JedliksToyCar {
     public String distanceDisplay() {
         JedliksToyCar car = new JedliksToyCar();
         return car.driven;
-
     }
 
     public String batteryDisplay() {
-        throw new UnsupportedOperationException("Please implement the JedliksToyCar.batteryDisplay()  method");
+        JedliksToyCar car1 = new JedliksToyCar();
+        return car1.battery;
     }
 
     public void drive() {
