256c8870803f9ca5db8af6245ac04d7339f10d31
KAMO333 <mmopanemoroka@gmail.com>
2026-02-15 20:58:13 +0200

    Add locomotive engineer functions and tests; implement raindrops exercise
    - Created `locomotive_engineer.py` with functions to manage wagons and routes.
    - Added unit tests for locomotive engineer functions in `locomotive_engineer_test.py`.
    - Introduced `pytest.ini` for test configuration.
    - Added raindrops exercise with implementation files: `raindrops.py`, `raindrops_test.py`, and associated metadata and help documentation.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,11 +1,18 @@
 def response(hey_bob):
-    
-    remove_spaces = hey_bob.replace(" ", "")
 
-    if remove_spaces.endswith("?"):
-        return "Sure."
+    clean_input = hey_bob.strip()
     
-    if remove_spaces.upper():
+    if clean_input == "":
+        return "Fine. Be that way!"
+    
+    if clean_input.isupper() and clean_input[-1] == "?":
+        return "Calm down, I know what I'm doing!"
+    
+    if clean_input.isupper(): 
         return "Whoa, chill out!"
     
-
+    if clean_input.endswith("?"):
+        return "Sure."
+    
+    else:
+        return "Whatever."
