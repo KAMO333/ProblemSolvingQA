935beaee74e4cb9545c333ae62e39bf45b608344
KAMO333 <mmopanemoroka@gmail.com>
2026-02-18 14:24:40 +0200

    Implement recycling logic in recycle_me function to categorize items

--- python/Python Problems/codewares/recycle.py
+++ python/Python Problems/codewares/recycle.py
@@ -1,5 +1,17 @@
 def recycle_me(rubbish):
-    return
+    plastic = 0
+    glass = 0
+    card = 0
+
+    for item in rubbish:
+        if item > 0:
+            plastic += 1
+        if item < 0:
+            glass += 1
+        if item == 0:
+            card += 1
+
+    return (plastic, glass, card)
 
 
 print(recycle_me([5, -9, 0, 6, -84, -95, 15]), (3, 3, 1))
